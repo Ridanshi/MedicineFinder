f1695ffb9694f356c9fed7a4fc0e54a9968cb272
Ridanshi <ridanshiagarwal2@gmail.com>
2026-01-02 03:52:49 +0530

    Add axios instance with credentials

--- my_app/src/api/axios.js
+++ my_app/src/api/axios.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
-  withCredentials: true, // ← This MUST be true
-  headers: {
-    'Content-Type': 'application/json'
-  }
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true
 });
 
-export default axiosInstance;
+export default api;
